19d81c89b836936b68151cd808555706e1f5b4c2
杨旭中 <459290751@qq.com>
2019-04-19 15:36:38 +0800

    配置中心服务化和高可用

--- cloud-config-client/src/main/java/com/yxz/cloud/config/client/CloudConfigClientApplication.java
+++ cloud-config-client/src/main/java/com/yxz/cloud/config/client/CloudConfigClientApplication.java
@@ -2,8 +2,10 @@ package com.yxz.cloud.config.client;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class CloudConfigClientApplication {
 
     public static void main(String[] args) {
