e10a139a24c69dd3528f1570b3ffc569adfa2d84
杨旭中 <459290751@qq.com>
2019-04-23 18:00:54 +0800

    基于spring cloud 版本Finchley.RELEASE 的  config + bus

--- cloud-config-client/src/main/java/com/yxz/cloud/config/client/web/HelloController.java
+++ cloud-config-client/src/main/java/com/yxz/cloud/config/client/web/HelloController.java
@@ -1,10 +1,12 @@
 package com.yxz.cloud.config.client.web;
 
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RefreshScope//配置文件自动刷新
 public class HelloController {
 
     @Value("${demo.hello}")
